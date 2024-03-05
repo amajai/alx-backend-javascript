39ed800ea7aedd804775fe01f6f3119aa745bde3
Abdul <aabdulmajeed.isa@gmail.com>
2024-03-05 21:30:18 +0100

    task 100: 100-await.js done

---
+++ 0x01-ES6_promise/100-await.js
@@ -0,0 +1,14 @@
+import { uploadPhoto, createUser } from './utils';
+
+export default async function asyncUploadUser() {
+  return Promise
+    .all([uploadPhoto(), createUser()])
+    .then((results) => ({
+      photo: results[0],
+      user: results[1],
+    }))
+    .catch(() => ({
+      photo: null,
+      user: null,
+    }));
+}
