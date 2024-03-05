6a85bda0a55613767c13fd8125f30df3896ef451
Abdul <aabdulmajeed.isa@gmail.com>
2024-03-05 20:42:53 +0100

    task 3 fix

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,13 +1,13 @@
 import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
-  /* eslint-disable no-unused-vars */
-  Promise.all([uploadPhoto(), createUser()])
+  return Promise
+    .all([uploadPhoto(), createUser()])
     .then((response) => {
       const [photoResponse, userResponse] = response;
       console.log(`${photoResponse.body} ${userResponse.firstName} ${userResponse.lastName}`);
     })
-    .catch((err) => {
+    .catch(() => {
       console.error('Signup system offline');
     });
 }
