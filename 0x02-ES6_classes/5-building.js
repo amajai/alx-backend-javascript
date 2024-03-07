5e7519a178d05647cad7ead1db449b614bf534cc
Abdul <aabdulmajeed.isa@gmail.com>
2024-03-07 14:58:51 +0100

    task 5: 5-building.js done

---
+++ 0x02-ES6_classes/5-building.js
@@ -0,0 +1,22 @@
+export default class Building {
+  /* eslint-disable no-underscore-dangle */
+  constructor(sqft) {
+    this._sqft = sqft;
+    if (this.constructor !== Building
+        && this.evacuationWarningMessage === Building.prototype.evacuationWarningMessage) {
+      throw new Error('Class extending Building must override evacuationWarningMessage');
+    }
+  }
+
+  get sqft() {
+    return this._sqft;
+  }
+
+  set sqft(value) {
+    this._sqft = value;
+  }
+
+  static evacuationWarningMessage() {
+    throw new Error('Class extending Building must override evacuationWarningMessage');
+  }
+}
