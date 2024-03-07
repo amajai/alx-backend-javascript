83b9bad63285a72d0423333743f40bf1ff453884
Abdul <aabdulmajeed.isa@gmail.com>
2024-03-07 16:35:52 +0100

    task 10: 10-car.js done

---
+++ 0x02-ES6_classes/10-car.js
@@ -0,0 +1,17 @@
+/* eslint-disable no-underscore-dangle */
+export default class Car {
+  constructor(brand, motor, color) {
+    this._brand = brand;
+    this._motor = motor;
+    this._color = color;
+  }
+
+  cloneCar() {
+    const clone = new this.constructor();
+    const symbols = Object.getOwnPropertySymbols(this);
+    symbols.forEach((symbol) => {
+      clone[symbol] = this[symbol];
+    });
+    return clone;
+  }
+}
