c4bd109bd6398084577379b0b4db5f48c18d7895
Abdul <aabdulmajeed.isa@gmail.com>
2024-03-11 22:56:12 +0100

    task 8: 8-clean_set.js done

---
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -0,0 +1,9 @@
+export default function cleanSet(set, startString) {
+  let str = '';
+  for (const value of set) {
+    if (startString && value.startsWith(startString)) {
+      str += (str.length === 0 ? '' : '-') + value.slice(startString.length);
+    }
+  }
+  return str;
+}
