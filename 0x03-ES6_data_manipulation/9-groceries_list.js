25f511558053135365bda47a28db1396cbd92d19
Abdul <aabdulmajeed.isa@gmail.com>
2024-03-12 09:44:26 +0100

    task 9: 9-groceries_list.js done

---
+++ 0x03-ES6_data_manipulation/9-groceries_list.js
@@ -0,0 +1,9 @@
+export default function groceriesList() {
+  const mapped = new Map();
+  mapped.set('Apples', 10);
+  mapped.set('Tomatoes', 10);
+  mapped.set('Pasta', 1);
+  mapped.set('Rice', 1);
+  mapped.set('Banana', 5);
+  return mapped;
+}
